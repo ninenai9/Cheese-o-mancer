e8823171fde283b008c523e4d6133c9b9020b139
MarcGonzalezCarro <marcg8236@gmail.com>
2026-03-25 10:32:11 +0100

    Temp Fix

--- include/Render.h
+++ include/Render.h
@@ -3,7 +3,7 @@
 #include "Module.h"
 #include "Vector2D.h"
 #include "SDL3/SDL.h"
-#include <SDL3_ttf/SDL_ttf.h>
+//#include <SDL3_ttf/SDL_ttf.h>
 
 class Render : public Module
 {
@@ -50,5 +50,5 @@ public:
 
 private:
 	bool vsync = false;
-	TTF_Font* font;
+	//TTF_Font* font;
 };
