83c5ec1dd1c4a50cbeb2061084284a56009792ad
Katy <sofiagiva1@gmail.com>
2026-04-16 16:21:55 +0200

    basic spritesheet moves jester

---
+++ assets/Textures/Spritesheets/Jester/2x3/j_sp.tsx
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="2x3" tilewidth="256" tileheight="384" tilecount="30" columns="10">
+ <image source="2x3.png" width="2562" height="1156"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="run"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="jump"/>
+  </properties>
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="hoponcheese"/>
+  </properties>
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
